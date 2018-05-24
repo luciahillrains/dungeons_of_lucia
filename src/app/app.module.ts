f58585ee1529e3929b6656f265f2936744facb0b
luciahillrains <luciahillrains@gmail.com>
2018-05-24 07:05:58 -0500

    skeleton of ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TileComponent } from './tile/tile.component';
+import { GameAreaComponent } from './game-area/game-area.component';
+import { TitleComponent } from './title/title.component';
+import { ConsoleComponent } from './console/console.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TileComponent,
+    GameAreaComponent,
+    TitleComponent,
+    ConsoleComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule
